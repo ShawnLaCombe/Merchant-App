84f26ba12cad16f48bdc42b46603ea23fb727587
Shawn <lacombeshawn@yahoo.com>
2016-08-03 10:26:49 -0500

    Add admin namespace and updated all links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    resources :products
+    resources :products
+    resources :brands
+    resources :categories
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -51,6 +59,5 @@ Rails.application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  #     #   end
 end
