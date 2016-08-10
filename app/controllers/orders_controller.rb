fcdccde3450e7ee1e7462a1472fff7f94b5a85d1
Shawn <lacombeshawn@yahoo.com>
2016-08-10 11:04:49 -0500

    Modify Orders Controller and Model, setup the create order methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,11 +8,35 @@ class OrdersController < ApplicationController
       redirect_to shop_url, notice: "Your cart is empty."
       return
     end
-    
+
     @order = Order.new
     @order.user_id = current_user.id
   end
 
   def show
   end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.add_line_items_from_cart(@cart)
+    
+    if @order.save
+      Cart.destroy(session[:cart_id])
+      session[:cart_id]= nil
+      redirect_to shop_url, notice: "Thanks for ordering from Store Tastic!"
+    else
+      render :new
+    end
+
+
+  end
+
+private 
+
+def order_params
+  params.require(:order).permit(:name, :address, :pay_type, :user_id)
+end
+
+
 end
